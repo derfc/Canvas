32ddbef6f0ee6153dbbc3c9ff67fa0b23e35b622
fred cheung <fredcheung@live.hk>
2021-01-31 00:49:50 +0800

    sat night update

--- javascript/eraser.js
+++ javascript/eraser.js
@@ -2,18 +2,16 @@
  * Drawing Line Functionality
  * ==================================
  ***********************************************/
-class DrawingLine extends PaintFunction {
+class Eraser extends PaintFunction {
   // This class extends the PaintFunction class
   constructor(contextReal) {
     super();
     this.context = contextReal;
-    this.color = "Black";
-    // this.color = colorStroke;
   }
 
   // On mouse down, ensure that the pen has these features
   onMouseDown(coord, event) {
-    this.context.strokeStyle = `${this.color}`;
+    this.context.strokeStyle = "#FFF";
     this.context.lineJoin = "round";
     this.context.lineWidth = 5;
     this.context.beginPath();
@@ -28,10 +26,6 @@ class DrawingLine extends PaintFunction {
   onMouseUp() {}
   onMouseLeave() {}
   onMouseEnter() {}
-
-  color(color) {
-    this.color = color;
-  }
 
   draw(x, y) {
     this.context.lineTo(x, y);
