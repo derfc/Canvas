118dd72e7de03abc354b80c17c675f5ac4578dd5
derfc <fredcheung@live.hk>
2021-02-01 19:27:34 +0800

    im back

---
+++ javascript/utilities.js
@@ -0,0 +1,30 @@
+"use strict";
+// COLOR, TEXT SIZE & STROKESIZE
+let canvasSettings = {
+  colorStroke: $("#stroke-color").val(),
+  colorFill: $("#fill-color").val(),
+  strokeSize: $("#stroke-size").val(),
+  // textSize
+  // textFont
+};
+
+$("#stroke-color")[0].oninput = function () {
+  canvasSettings.colorStroke = this.value;
+};
+
+$("#fill-color")[0].oninput = function () {
+  canvasSettings.colorFill = this.value;
+};
+
+$("#stroke-size")[0].oninput = function () {
+  canvasSettings.strokeSize = this.value;
+};
+
+// $("#text-size")[0].oninput = function () {
+//   canvasSettings.textsize = this.value;
+// };
+
+$("#clear-canvas").click(() => {
+  contextReal.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+  contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+});
