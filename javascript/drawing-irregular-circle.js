da7b0ea677c0a976d79f3612e88043fe900c3f04
derfc <fredcheung@live.hk>
2021-02-02 13:42:42 +0800

    common center ctrl, circle, poly side btn ut

--- javascript/drawing-irregular-circle.js
+++ javascript/drawing-irregular-circle.js
@@ -4,127 +4,166 @@ class DrawingIrregularCircle extends PaintFunction {
 		this.contextReal = contextReal;
 		this.contextDraft = contextDraft;
 		this.click = 0;
+		this.contextDraft.lineWidth = canvasSettings.strokeSize;
+		this.contextDraft.fillStyle = canvasSettings.colorFill;
+		this.contextDraft.strokeStyle = canvasSettings.colorStroke;
+		this.contextReal.lineWidth = canvasSettings.strokeSize;
+		this.contextReal.fillStyle = canvasSettings.colorFill;
+		this.contextReal.strokeStyle = canvasSettings.colorStroke;
 	}
 
 	onMouseDown(coord, event) {
-		this.contextDraft.fillStyle = "black";
-		this.contextDraft.strokeStyle = "black";
-	}
-
-	onDragging(coord, event) {}
-
-	onMouseMove(coord) {
-		if (this.click == 1) {
-			if (regularFix) {
-				this.contextDraft.lineJoin = "round";
-				this.contextDraft.clearRect(
-					0,
-					0,
-					canvasDraft.width,
-					canvasDraft.height
-				);
-				let xDiv = coord[0] - this.origX;
-				let yDiv = coord[1] - this.origY;
-				let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
-				let radius = 0.5 * Math.sqrt(xDiv ** 2 + yDiv ** 2);
-				// let radiusX = Math.abs(xDiv) / 2;
-				// let radiusY = Math.abs(yDiv) / 2;
-				this.contextDraft.beginPath();
-				this.contextDraft.arc(
-					center[0],
-					center[1],
-					radius,
-					0,
-					2 * Math.PI,
-					false
-				);
-				this.contextDraft.stroke();
-			} else {
-				this.contextDraft.lineJoin = "round";
-				this.contextDraft.clearRect(
-					0,
-					0,
-					canvasDraft.width,
-					canvasDraft.height
-				);
-				let xDiv = coord[0] - this.origX;
-				let yDiv = coord[1] - this.origY;
-				let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
-				// let radius = 0.5 * Math.sqrt(xDiv ** 2 + yDiv ** 2);
-				let radiusX = Math.abs(xDiv) / 2;
-				let radiusY = Math.abs(yDiv) / 2;
-				this.contextDraft.beginPath();
-				this.contextDraft.ellipse(
-					center[0],
-					center[1],
-					radiusX,
-					radiusY,
-					0,
-					2 * Math.PI,
-					false
-				);
-				this.contextDraft.stroke();
-			}
-		}
-	}
-	onMouseUp(coord) {
 		if (this.click == 0) {
 			this.origX = coord[0];
 			this.origY = coord[1];
 			this.click++;
-		} else if (this.click == 1) {
-			if (regularFix) {
-				this.contextReal.lineJoin = "round";
-				this.contextDraft.clearRect(
-					0,
-					0,
-					canvasDraft.width,
-					canvasDraft.height
-				);
-				let xDiv = coord[0] - this.origX;
-				let yDiv = coord[1] - this.origY;
-				let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
-				let radius = 0.5 * Math.sqrt(xDiv ** 2 + yDiv ** 2);
-				// let radiusX = Math.abs(xDiv) / 2;
-				// let radiusY = Math.abs(yDiv) / 2;
-				this.contextReal.beginPath();
-				this.contextReal.arc(
-					center[0],
-					center[1],
-					radius,
-					0,
-					2 * Math.PI,
-					false
-				);
-				this.contextReal.stroke();
-				this.click = 0;
-			} else {
-				this.contextReal.lineJoin = "round";
-				this.contextDraft.clearRect(
-					0,
-					0,
-					canvasDraft.width,
-					canvasDraft.height
-				);
-				let xDiv = coord[0] - this.origX;
-				let yDiv = coord[1] - this.origY;
-				let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
-				// let radius = 0.5 * Math.sqrt(xDiv ** 2 + yDiv ** 2);
-				let radiusX = Math.abs(xDiv) / 2;
-				let radiusY = Math.abs(yDiv) / 2;
-				this.contextReal.beginPath();
-				this.contextReal.ellipse(
-					center[0],
-					center[1],
-					radiusX,
-					radiusY,
-					0,
-					2 * Math.PI,
-					false
-				);
-				this.contextReal.stroke();
-				this.click = 0;
-			}
+		}
+	}
+
+	onDragging(coord, event) {
+		if (centerFix && regularFix) {
+			this.contextDraft.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX, this.origY];
+			let radius = Math.sqrt(xDiv ** 2 + yDiv ** 2);
+			this.contextDraft.beginPath();
+			this.contextDraft.arc(
+				center[0],
+				center[1],
+				radius,
+				0,
+				2 * Math.PI,
+				false
+			);
+			this.contextDraft.stroke();
+			this.click = 0;
+		} else if (centerFix && !regularFix) {
+			this.contextDraft.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX, this.origY];
+			let radiusX = Math.abs(xDiv);
+			let radiusY = Math.abs(yDiv);
+			this.contextDraft.beginPath();
+			this.contextDraft.ellipse(
+				center[0],
+				center[1],
+				radiusX,
+				radiusY,
+				0,
+				2 * Math.PI,
+				false
+			);
+			this.contextDraft.stroke();
+			this.click = 0;
+		} else if (!centerFix && regularFix) {
+			this.contextDraft.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
+			let radius = 0.5 * Math.sqrt(xDiv ** 2 + yDiv ** 2);
+			this.contextDraft.beginPath();
+			this.contextDraft.arc(
+				center[0],
+				center[1],
+				radius,
+				0,
+				2 * Math.PI,
+				false
+			);
+			this.contextDraft.stroke();
+			this.click = 0;
+		} else {
+			this.contextDraft.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
+			let radiusX = Math.abs(xDiv) / 2;
+			let radiusY = Math.abs(yDiv) / 2;
+			this.contextDraft.beginPath();
+			this.contextDraft.ellipse(
+				center[0],
+				center[1],
+				radiusX,
+				radiusY,
+				0,
+				2 * Math.PI,
+				false
+			);
+			this.contextDraft.stroke();
+			this.click = 0;
+		}
+	}
+
+	onMouseMove(coord) {}
+	onMouseUp(coord) {
+		if (centerFix && regularFix) {
+			this.contextReal.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX, this.origY];
+			let radius = Math.sqrt(xDiv ** 2 + yDiv ** 2);
+			this.contextReal.beginPath();
+			this.contextReal.arc(center[0], center[1], radius, 0, 2 * Math.PI, false);
+			this.contextReal.stroke();
+			this.click = 0;
+		} else if (centerFix && !regularFix) {
+			this.contextReal.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX, this.origY];
+			let radiusX = Math.abs(xDiv);
+			let radiusY = Math.abs(yDiv);
+			this.contextReal.beginPath();
+			this.contextReal.ellipse(
+				center[0],
+				center[1],
+				radiusX,
+				radiusY,
+				0,
+				2 * Math.PI,
+				false
+			);
+			this.contextReal.stroke();
+			this.click = 0;
+		} else if (!centerFix && regularFix) {
+			this.contextReal.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
+			let radius = 0.5 * Math.sqrt(xDiv ** 2 + yDiv ** 2);
+			this.contextReal.beginPath();
+			this.contextReal.arc(center[0], center[1], radius, 0, 2 * Math.PI, false);
+			this.contextReal.stroke();
+			this.click = 0;
+		} else {
+			this.contextReal.lineJoin = "round";
+			this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+			let xDiv = coord[0] - this.origX;
+			let yDiv = coord[1] - this.origY;
+			let center = [this.origX + xDiv / 2, this.origY + yDiv / 2];
+			let radiusX = Math.abs(xDiv) / 2;
+			let radiusY = Math.abs(yDiv) / 2;
+			this.contextReal.beginPath();
+			this.contextReal.ellipse(
+				center[0],
+				center[1],
+				radiusX,
+				radiusY,
+				0,
+				2 * Math.PI,
+				false
+			);
+			this.contextReal.stroke();
+			this.click = 0;
 		}
 	}
 	onMouseLeave() {}
--- javascript/utilities.js
+++ javascript/utilities.js
@@ -1,23 +1,28 @@
 "use strict";
 // COLOR, TEXT SIZE & STROKESIZE
 let canvasSettings = {
-  colorStroke: $("#stroke-color").val(),
-  colorFill: $("#fill-color").val(),
-  strokeSize: $("#stroke-size").val(),
-  // textSize
-  // textFont
+	colorStroke: $("#stroke-color").val(),
+	colorFill: $("#fill-color").val(),
+	strokeSize: $("#stroke-size").val(),
+	polygonSides: $("#poly-sides").val(),
+	// textSize
+	// textFont
 };
 
 $("#stroke-color")[0].oninput = function () {
-  canvasSettings.colorStroke = this.value;
+	canvasSettings.colorStroke = this.value;
 };
 
 $("#fill-color")[0].oninput = function () {
-  canvasSettings.colorFill = this.value;
+	canvasSettings.colorFill = this.value;
 };
 
 $("#stroke-size")[0].oninput = function () {
-  canvasSettings.strokeSize = this.value;
+	canvasSettings.strokeSize = this.value;
+};
+
+$("#poly-sides")[0].oninput = function () {
+	canvasSettings.polygonSides = this.value;
 };
 
 // $("#text-size")[0].oninput = function () {
@@ -25,13 +30,13 @@ $("#stroke-size")[0].oninput = function () {
 // };
 
 $("#clear-canvas").click(() => {
-  contextReal.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-  contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+	contextReal.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+	contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
 });
 
 ////////////DOM
 $(".icons").click(function (e) {
-  console.log(e);
-  console.log(this);
-  this.classList.remove("active");
+	console.log(e);
+	console.log(this);
+	this.classList.remove("active");
 });
